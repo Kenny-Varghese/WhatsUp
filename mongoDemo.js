28a598e794d8d2e5ce3ba61d0bde05a6fc338d2d
rohansaxena658 <saxena24@purdue.edu>
2018-11-30 14:04:15 -0500

    Mongo Demo Tutorial Functions (createCollection, insertOne, findOne)

--- mongoDemo.js
+++ mongoDemo.js
@@ -1,10 +1,40 @@
-var MongoClient = require('mongodb').MongoClient
-var url = "mongodb://localhost:27017/mydb"
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, { useNewUrlParser: true }, function(err,db){
       if(err){
             throw err;
       }
       console.log("Database created!");
-      db.close();
+      var dbo = db.db("mydb");
+      var insertObj = {name: "Rohan Saxena", age: 20 };
+
+      //This is how to create a collection with name users
+      dbo.createCollection("users", function(err,res){
+            if(err){
+                  throw err;
+            }
+            console.log("Collection Created!");
+            db.close();
+      });
+
+      //This is how to insert an object
+      dbo.collection("users").insertOne(insertObj, function(err,res){
+            if(err){
+                  throw err;
+            }
+            console.log("1 object inserted!");
+            db.close();
+      })
+
+      //This is how to find an object
+      dbo.collection("users").findOne({}, function(err,result){
+            if(err){
+                  throw err;
+            }
+            console.log(result.name);
+            console.log(result.age);
+      });
+
+
 });
