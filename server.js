7cf35028713cc6ba8bbd4c68b1dcf948341a917d
rohansaxena658 <saxena24@purdue.edu>
2018-12-06 04:17:33 -0500

    database lowkey working

--- server.js
+++ server.js
@@ -1,55 +1,59 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var url = "mongodb://localhost:27017/users"
+var url = "mongodb://localhost:27017"
 var app = express();
 var port = process.env.PORT || 3035;
-// var open = require('open');
 app.use(express.static(__dirname));
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect(url,{ useNewUrlParser: true }, function(err,db){
-      console.log("Connected to database");
-});
-
-var login = mongoose.model('Login', {username_login: String, password_login: String});
-var message = mongoose.model('Message', {username_login: String, messageString: String});
-var signup = mongoose.model('Signup', {name: String, email: String, username_login: String, password_login: String})
+mongoose.connect('mongodb://localhost:27017/logins',{ useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 
 app.get("/addname", function(req,res) {
       login.find({}, (err, login)=> {
             res.send(login);
       })
-          
-//  
 });
 
 app.post("/addname2", (req, res) => {
-    var myData = new login(req.body);
-    console.log("test 1");
-    console.log(req.body);
-    res.redirect('loginPage/loginPage.html');
+      req.body.username_login;
+      req.body.password_login;
+      var myData = new login(req.body);
+      console.log("test 1");
+      console.log(req.body);
+      res.redirect('loginPage/loginPage.html');
 
 });
 app.get("/signup", function(req,res) {
-    signup.find({}, (err, login)=> {
-          res.send(signup);
-    })
-        
-//  
+      signup.find({}, (err, login)=> {
+            res.send(signup);
+      });
 });
 
 app.post("/signup", (req, res) => {
-  var myData = new signup(req.body);
-  console.log("test 1");
-  console.log(req.body);
-  res.redirect('loginPage/loginPage.html');
+      db.once('open', function() {
+            console.log("Connected to database");
+            var loginSchema = mongoose.Schema({
+                  username: String,
+                  password: String
+            });
+            var Login = mongoose.model('login', loginSchema, 'users');
+            var newUser = new Login({username: req.body.username_signup, password: req.body.password_signup});
+            newUser.save(function (err,userInfo){
+                  if (err){
+                        return console.error(err);
+                  }
+                  console.log(userInfo.username + " saved to users collection");
+            });
+      });
+      res.redirect('loginPage/loginPage.html');
 
 });
 
 
 app.listen(port, function(){
-    console.log("yayyy working!");
+      console.log("yayyy working!");
 });
