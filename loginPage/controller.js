b8464f68cf07aa68fa85d6a646ff905efecfad35
rohansaxena658 <saxena24@purdue.edu>
2018-12-06 03:01:41 -0500

    displaying message on message board

--- loginPage/controller.js
+++ loginPage/controller.js
@@ -12,11 +12,30 @@ var containerDiv = document.getElementById('container');
 function clicked(){
       let msgButton = document.getElementById('sendMessageBtn');
       var temp = document.getElementById('messageInput').value;
+
       console.log("ur mom sucks");
       console.log(temp);
       if(temp.length == 0){
             return;
       }
+      let div = document.createElement('div');
+      div.classList.add('card');
+      div.classList.add('hoverable');
+      let div2 = document.createElement('div');
+      div2.classList.add('card-content');
+      let spanTitle = document.createElement('span');
+      spanTitle.classList.add('card-title');
+      spanTitle.innerHTML = temp;
+      document.getElementById('messageInput').value = '';
+      // spanTitle.innerHTML = `${msg.id} - ${msg.message}`;
+      // if (id && msg.id === id) {
+      //       div.classList.add('blue-grey');
+      //       div.classList.add('darken-1');
+      //       div2.classList.add('white-text');
+      // }
+      div2.appendChild(spanTitle);
+      div.appendChild(div2);
+      collection.appendChild(div);
       // socket.on('chat message' (msg:Message) => {
       //       this.newMessage(msg);
       // });
